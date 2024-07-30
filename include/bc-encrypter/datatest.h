f28ea4782de24705740142dc5d67249aa6489a6b
0xabin <abinbvinod@gmail.com>
2024-07-30 12:33:30 +0530

    Header File tested

---
+++ include/bc-encrypter/datatest.h
@@ -0,0 +1,11 @@
+//
+// Created by 0xAbin on 30/07/24.
+//
+
+#ifndef BC_ENCYPTER_DATATEST_H
+#define BC_ENCYPTER_DATATEST_H
+
+// Define the array
+const int arr[] = {6,2,4,5,6,7,8,9};
+
+#endif //BC_ENCYPTER_DATATEST_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,15 @@
 // Created by 0xAbin on 27/07/24.
 //
 #include <iostream>
+#include "bc-encrypter/datatest.h"
 
 int main() {
-    std::cout << "Project main" << std::endl;
+    std::cout << "Project Node" << std::endl;
+
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+        std::cout << arr[i] << "good";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
